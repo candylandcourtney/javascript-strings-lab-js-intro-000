aa41e6841e4354bb2c794005a17f723f680a1fcc
Courtney Little <courtneylittle6@gmail.com>
2018-12-18 04:11:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + SpecialGuest + "!";
+var greetSpecialGuest = `hello` + SpecialGuest + "!";
 
 var topic = "space";
 
